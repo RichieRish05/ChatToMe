0b5b3907b90d0ce9ccc0f9baf9b32e428c57aa4f
Rishi Murumkar <108751289+RichieRish05@users.noreply.github.com>
2025-05-21 02:54:42 -0700

    Rebuilding chroma on startup every time

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from query import query_model
+from create_db import generate_data_store
 
+generate_data_store()
 app = FastAPI()
 
 class Question(BaseModel):
